21d32725cc68c5fbc74a99913dd74d2a2d968142
สามารถ จงนอก <pea5815@gmail.com>
2022-04-27 10:17:01 +0700

    chang house view
    chang house view

--- house_view.php
+++ house_view.php
@@ -1,3 +1,56 @@
+<?php require_once('Connections/harry.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$colname_student = "-1";
+if (isset($_GET['houseid'])) {
+  $colname_student = $_GET['houseid'];
+}
+mysql_select_db($database_harry, $harry);
+$query_student = sprintf("SELECT * FROM student WHERE house_id = %s", GetSQLValueString($colname_student, "int"));
+$student = mysql_query($query_student, $harry) or die(mysql_error());
+$row_student = mysql_fetch_assoc($student);
+$totalRows_student = mysql_num_rows($student);
+
+$colname_house = "-1";
+if (isset($_GET['houseid'])) {
+  $colname_house = $_GET['houseid'];
+}
+mysql_select_db($database_harry, $harry);
+$query_house = sprintf("SELECT * FROM house WHERE house_id = %s", GetSQLValueString($colname_house, "int"));
+$house = mysql_query($query_house, $harry) or die(mysql_error());
+$row_house = mysql_fetch_assoc($house);
+$totalRows_house = mysql_num_rows($house);
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -20,11 +73,9 @@
 	<div class="row">
 		<div class="col-md-12">
 			<h2>
-				Heading
-			</h2>
+		    <?php echo $row_house['house_name']; ?> </h2>
 			<p>
-				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-			</p>
+			<?php echo $row_house['house_detail']; ?>. </p>
 			<p>
 				<a class="btn" href="#">View details »</a>
 			</p>
@@ -37,3 +88,8 @@
     <script src="js/scripts.js"></script>
   </body>
 </html>
+<?php
+mysql_free_result($student);
+
+mysql_free_result($house);
+?>
