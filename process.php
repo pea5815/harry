b64b63534403dcffb3ed7dfe223fa8015155563a
สามารถ จงนอก <pea5815@gmail.com>
2022-04-27 12:16:49 +0700

    add file
    add file

--- process.php
+++ process.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <?php require_once('Connections/harry.php'); ?>
 <?php
 if (!function_exists("GetSQLValueString")) {
@@ -40,10 +43,21 @@ $query_student = sprintf("SELECT * FROM student WHERE student_fullname = %s", Ge
 $student = mysql_query($query_student, $harry) or die(mysql_error());
 $row_student = mysql_fetch_assoc($student);
 $totalRows_student = mysql_num_rows($student);
+
+$colname_house = "-1";
+if (isset($_SESSION['hid'])) {
+  $colname_house = $_SESSION['hid'];
+}
+mysql_select_db($database_harry, $harry);
+$query_house = sprintf("SELECT * FROM house WHERE house_id = %s", GetSQLValueString($colname_house, "int"));
+$house = mysql_query($query_house, $harry) or die(mysql_error());
+$row_house = mysql_fetch_assoc($house);
+$totalRows_house = mysql_num_rows($house);
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -56,65 +70,44 @@ $totalRows_student = mysql_num_rows($student);
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 
-  </head>
-  <body>
+</head>
+
+<body>
 
     <div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			<div class="jumbotron">
-				<h2>
-				Hello, <?php echo $row_student['student_fullname']; ?> </h2>
-				<p>ยินดีด้วย คุณถูกเลือกให้อยู่ในบ้าน .
-				</p>
-				<p>
-					<a class="btn btn-primary btn-large" href="#">Learn more</a>
-				</p>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-4">
-			<h2>
-				Heading
-			</h2>
-			<p>
-				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-			</p>
-			<p>
-				<a class="btn" href="#">View details »</a>
-			</p>
-		</div>
-		<div class="col-md-4">
-			<h2>
-				Heading
-			</h2>
-			<p>
-				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-			</p>
-			<p>
-				<a class="btn" href="#">View details »</a>
-			</p>
-		</div>
-		<div class="col-md-4">
-			<h2>
-				Heading
-			</h2>
-			<p>
-				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-			</p>
-			<p>
-				<a class="btn" href="#">View details »</a>
-			</p>
-		</div>
-	</div>
-</div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="jumbotron">
+                    <h2>
+                        Hello, <?php echo $row_student['student_fullname']; ?> </h2>
+                    <p>ยินดีด้วย คุณถูกเลือกให้อยู่ในบ้าน 
+                        <?php 
+                        $_SESSION["hid"] =$row_student['house_id']; 
+                        //echo $_SESSION['hid'];
+                        ?>.
+
+                    
+                    <?php echo $row_house['house_name']; ?></p>
+                    <form name="form1" method="post" action="">
+                      <input name="hiddenField" type="hidden" id="hiddenField" value="<?php echo $row_student['house_id']; ?>">
+                    </form>
+                    <p>
+                        <a class="btn btn-primary btn-large" href="index.php">หน้าหลัก</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
 
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/scripts.js"></script>
-  </body>
+</body>
+
 </html>
 <?php
 mysql_free_result($student);
+
+mysql_free_result($house);
 ?>
