71e2bf1dc99ee173b7a2b804b98e2f7115a9e3c4
สามารถ จงนอก <pea5815@gmail.com>
2022-04-27 09:59:27 +0700

    chang file
    chang file

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-3">
 			<h2>
 				Heading
 			</h2>
@@ -44,7 +44,7 @@
 				<a class="btn" href="#">View details »</a>
 			</p>
 		</div>
-		<div class="col-md-4">
+		<div class="col-md-3">
 			<h2>
 				Heading
 			</h2>
@@ -55,7 +55,18 @@
 				<a class="btn" href="#">View details »</a>
 			</p>
 		</div>
-		<div class="col-md-4">
+		<div class="col-md-3">
+			<h2>
+				Heading
+			</h2>
+			<p>
+				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+			</p>
+			<p>
+				<a class="btn" href="#">View details »</a>
+			</p>
+		</div>
+        <div class="col-md-3">
 			<h2>
 				Heading
 			</h2>
